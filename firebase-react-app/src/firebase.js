1920cd4f752beb5405a720fc63631653f10d1b3e
Alii <141956458+AlsalmanA@users.noreply.github.com>
2023-10-07 17:53:40 -0400

    signin / registration connected to firebase auth

--- firebase-react-app/src/firebase.js
+++ firebase-react-app/src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 //import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set } from "firebase/database";
+import { get, getDatabase, ref, set } from "firebase/database";
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyDl5jy0QAcfbGUvp3Xom2lwQSJiZ0ziOrg",
@@ -16,6 +17,11 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Authentication
+export const auth = getAuth(app)
+
+
 //const analytics = getAnalytics(app);
 
 // Initialize Database
--- firebase-react-app/src/pages/SignInPage.js
+++ firebase-react-app/src/pages/SignInPage.js
@@ -1,54 +1,102 @@
-import React, { useState } from 'react';
-import { getDatabase, ref, set } from 'firebase/database';
-import './SignInPage.css'; // Importing styles
+import React, { useState, useEffect } from 'react';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'; 
+import { auth } from './../firebase';
+import './SignInPage.css';
 
-const SignInPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function App() {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [message, setMessage] = useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const db = getDatabase();
-    const usersRef = ref(db, 'users');  
-
-    // Generate a unique reference for the new user data
-    const newUserRef = ref(usersRef);
-
-    await set(newUserRef, { 
-      email: email,
-      password: password  // IMPORTANT: Store passwords securely (hashed & salted) in production apps!
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+        setCurrentUser(user);
     });
 
-    alert('User details saved to Firebase!');
-    setEmail('');  
-    setPassword('');
+    return () => unsubscribe();
+  }, []);
+
+  const register = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      setCurrentUser(userCredential.user);
+      setMessage("Successfully registered!");
+
+      setRegisterEmail("");
+      setRegisterPassword("");
+      
+    } catch (error) {
+      setMessage(`Registration Error: ${error.message}`);
+    }
+  };
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+      setMessage("Successfully logged in!");
+
+      setLoginEmail("");
+      setLoginPassword("");
+      
+    } catch (error) {
+      setMessage(`Login Error: ${error.message}`);
+    }
+  };
+
+  const logout = async() => {
+    try {
+      await signOut(auth);
+      setMessage("Successfully logged out!");
+    } catch (error) {
+      setMessage(`Logout Error: ${error.message}`);
+    }
   };
 
   return (
     <div className="signin-container">
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit} className="signin-form">
+      <div className="signin-form">
+        <h2>Register</h2>
         <div className="input-container">
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" value={email} onChange={handleEmailChange} placeholder="Enter your email" />
+          <label>Email:</label>
+          <input type="email" value={registerEmail} onChange={(e) => setRegisterEmail(e.target.value)} placeholder="Enter your email for registration" />
+        </div>
+
+        <div className="input-container">
+          <label>Password:</label>
+          <input type="password" value={registerPassword} onChange={(e) => setRegisterPassword(e.target.value)} placeholder="Enter your password for registration" />
+        </div>
+        <button className="signin-btn" onClick={register}>Register</button>
+      </div>
+
+      <div className="signin-form">
+        <h2>Login</h2>
+        <div className="input-container">
+          <label>Email:</label>
+          <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} placeholder="Enter your email" />
         </div>
         <div className="input-container">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" value={password} onChange={handlePasswordChange} placeholder="Enter your password" />
+          <label>Password:</label>
+          <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} placeholder="Enter your password" />
         </div>
-        <button type="submit" className="signin-btn">Sign In</button>
-      </form>
+        <button className="signin-btn" onClick={login}>Login</button>
+      </div>
+
+      <div className="logout-section">
+        <h2>Logout</h2>
+        {currentUser && (
+          <span className="user-info">
+            Logged in as: {currentUser?.email}
+          </span>
+        )}
+        <button className="signin-btn" onClick={logout}>Logout</button>
+      </div>
+
+      {message && <div className="message">{message}</div>}
     </div>
   );
-};
+}
 
-export default SignInPage;
+export default App;
