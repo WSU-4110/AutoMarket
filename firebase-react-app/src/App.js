9da448a1ff0beae1c11d3b2ed962f8ab74cbc00b
azer232 <ghoddoussi.reza@gmail.com>
2023-10-12 18:13:22 -0400

    Created Sellers Page Test

--- firebase-react-app/src/App.js
+++ firebase-react-app/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path="signin" element={<SignInPage />} />
           <Route path="/" element={<HomePage />} index />
           <Route path="signup" element={<SignUpPage />} />
+          <Route path="/" element={<HomePage />} index />
+          <Route path="cart" element={<Cart />} />
         </Routes>
       </div>
     </Router>
