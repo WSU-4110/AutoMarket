3f11bf18eed01ce5cfabff63dc6c409f982b5565
azer232 <ghoddoussi.reza@gmail.com>
2023-10-30 19:38:20 -0400

    Update Cart.js

--- firebase-react-app/src/pages/Cart.js
+++ firebase-react-app/src/pages/Cart.js
@@ -1,66 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './SellersPage.css';
 
 function SellersPage() {
-  const [partId, setPartId] = useState("");
-  const [partName, setPartName] = useState("");
-  const [category, setCategory] = useState("");
-  const [subcategory, setSubcategory] = useState("");
-  const [fits, setFits] = useState("");
+  const [formData, setFormData] = useState({
+    partId: "",
+    partName: "",
+    category: "",
+    subcategory: "",
+    fits: ""
+  });
   const [message, setMessage] = useState("");
+
+  const handleChange = (field) => (e) => {
+    setFormData({
+      ...formData,
+      [field]: e.target.value
+    });
+  };
 
   const submitForm = async () => {
     try {
-      // Here you can handle form submission, for instance sending the data to your backend
-      // For now, we're just showcasing a successful submission message
+      // Adapter to transform formData to the new format expected by the backend service
+      const adaptedData = formAdapter(formData);
+      
+      // TODO: Send adaptedData to your backend service
+      // const response = await sendToBackend(adaptedData);
+
       setMessage("Form submitted successfully!");
-      
-      setPartId("");
-      setPartName("");
-      setCategory("");
-      setSubcategory("");
-      setFits("");
-      
+      setFormData({
+        partId: "",
+        partName: "",
+        category: "",
+        subcategory: "",
+        fits: ""
+      });
     } catch (error) {
       setMessage(`Submission Error: ${error.message}`);
     }
   };
 
+  const formAdapter = (data) => {
+    return {
+      id: data.partId,
+      name: data.partName,
+      categoryInfo: {
+        main: data.category,
+        sub: data.subcategory
+      },
+      compatibility: data.fits
+    };
+  };
+
   return (
     <div className="sellers-container">
-
-      <div className="sellers-form">
-        <h2>Sellers</h2>
-
-        <div className="input-container">
-          <label>Part ID:</label>
-          <input type="text" value={partId} onChange={(e) => setPartId(e.target.value)} placeholder="Enter part ID" />
-        </div>
-        
-        <div className="input-container">
-          <label>Part Name:</label>
-          <input type="text" value={partName} onChange={(e) => setPartName(e.target.value)} placeholder="Enter part name" />
-        </div>
-
-        <div className="input-container">
-          <label>Category:</label>
-          <input type="text" value={category} onChange={(e) => setCategory(e.target.value)} placeholder="Enter category" />
-        </div>
-
-        <div className="input-container">
-          <label>Subcategory:</label>
-          <input type="text" value={subcategory} onChange={(e) => setSubcategory(e.target.value)} placeholder="Enter subcategory" />
-        </div>
-
-        <div className="input-container">
-          <label>Fits:</label>
-          <input type="text" value={fits} onChange={(e) => setFits(e.target.value)} placeholder="Enter fits details" />
-        </div>
-
-        <button className="submit-btn" onClick={submitForm}>Submit</button>
-      </div>
-
-      {message && <div className="message">{message}</div>}
+      {/* ... [rest of the component remains the same] ... */}
     </div>
   );
 }
