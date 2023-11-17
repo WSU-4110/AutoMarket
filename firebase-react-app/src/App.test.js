c5385a3f6dbf97013ab6a776715c54dafb28e79f
Alii <141956458+AlsalmanA@users.noreply.github.com>
2023-11-17 16:16:17 -0500

    project header and functionality unit testing

--- firebase-react-app/src/App.test.js
+++ firebase-react-app/src/App.test.js
@@ -1,9 +1,89 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './Header';
+import { auth } from './firebase';
 
-test('renders learn react link', () => 
+jest.mock('./firebase', () => 
+({
+  auth: {
+    onAuthStateChanged: jest.fn(),
+    signOut: jest.fn(),
+  },
+}));
+
+const mockAuthStateChanged = (user) => 
 {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  auth.onAuthStateChanged.mockImplementationOnce(callback => 
+    {
+    callback(user);
+    return () => {}; 
+  });
+};
+
+const MockHeader = () => 
+(
+  <Router>
+    <Header />
+  </Router>
+);
+
+describe('Header Component', () => 
+{
+  test('shows sign-in button when not logged in', () => 
+  {
+    mockAuthStateChanged(null);
+    render(<MockHeader />);
+    expect(screen.getByText('Sign In')).toBeInTheDocument();
+  });
+
+  test('shows sign-up button when not logged in', () => 
+  {
+    mockAuthStateChanged(null);
+    render(<MockHeader />);
+    expect(screen.getByText('Sign Up')).toBeInTheDocument();
+  });
+
+  test('shows user dropdown when logged in', () => 
+  {
+    const testUser = { email: 'testing@gmail.com' };
+    mockAuthStateChanged(testUser);
+
+    render(<MockHeader />);
+
+    expect(screen.getByText(`Hello, ${testUser.email}`)).toBeInTheDocument();
+  });
+
+  test('dropdown toggle functionality', () => 
+  {
+    const testUser = { email: 'testing@gmail.com' };
+    mockAuthStateChanged(testUser);
+
+    render(<MockHeader />);
+
+    fireEvent.click(screen.getByText(`Hello, ${testUser.email}`));
+    expect(screen.getByText('Profile')).toBeInTheDocument();
+    expect(screen.getByText('Sign Out')).toBeInTheDocument();
+  });
+
+  test('sign out functionality', async () => 
+  {
+    const testUser = { email: 'testing@gmail.com' };
+    mockAuthStateChanged(testUser);
+    auth.signOut.mockResolvedValueOnce();
+
+    render(<MockHeader />);
+    fireEvent.click(screen.getByText(`Hello, ${testUser.email}`));
+    fireEvent.click(screen.getByText('Sign Out'));
+
+    expect(auth.signOut).toHaveBeenCalled();
+  });
+
+  test('navigates to home when the AutoMarket logo is clicked', () => 
+  {
+    mockAuthStateChanged(null);
+    render(<MockHeader />);
+  
+    fireEvent.click(screen.getByAltText('logo'));
+  });
 });
