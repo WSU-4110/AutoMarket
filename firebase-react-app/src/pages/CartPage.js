3baae128de0a3b97bc239ddfbc97f58ff22bdd9f
usmansaiyed02 <saiyedusman@gmail.com>
2023-11-30 17:16:18 -0500

    reorganized cart page

--- firebase-react-app/src/pages/CartPage.js
+++ firebase-react-app/src/pages/CartPage.js
@@ -1,68 +1,76 @@
-import Header from "./../Header";
-import React, { useState } from 'react';
+import React from 'react';
+import Header from './../Header';
 import './CartPage.css';
-import homeBreakPadsImage from "../images/homeBreakPadsImage.jpg"
-import homeHeadlightImage from "../images/homeHeadlightImage.jpg"
-import homeSparkImage from "../images/homeSparkImage.jpg"
-function CartPage() {
+import homeBreakPadsImage from '../images/homeBreakPadsImage.jpg';
+import homeHeadlightImage from '../images/homeHeadlightImage.jpg';
+import homeSparkImage from '../images/homeSparkImage.jpg';
 
+function CartPage() {
   return (
     <>
-    <Header/>
-  <div className="shoppingcart-container">
-    <div className='MyCart-container'>
-     <h1 title="cart-title">MY CART</h1>
-    </div>
+      <Header />
+      <div className="shoppingcart-container">
+        <div className="MyCart-container">
+        <h1 title="cart-title">MY <span>CART</span></h1>
+        </div>
 
-    <br></br>
+        <br />
 
-  <div className="product-container">
+        <div className="product-container">
+          <div className="order-summary-container">
+            <h2 title="order-title">Order Summary</h2>
+          </div>
+          <div className="order-total-container">
+            <h2 title="subtotal-title">Item(s) Subtotal:</h2>
+          </div>
 
-    <div className='order-summary-container'>
-      <h2 title='order-title'>Order Summary</h2>
-    </div>
-    <div className='order-total-container'>
-      <h2 title='subtotal-title'>Item(s) Subtotal:</h2> 
-    </div>
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeBreakPadsImage}
+                alt="Break Pads"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>Break Pads</h2>
+              <p>Part Number: 12345</p>
+            </div>
+          </div>
 
-    <div className="product">
-      <div className="product-image">
-        <img src={homeBreakPadsImage} alt="Break Pads"
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Break Pads</h2>
-        <p>Part Number: 12345</p>
-      </div>
-    </div>
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeHeadlightImage}
+                alt="Head Light"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>Morimoto XB LED Headlights</h2>
+              <p>Part Number: 67890</p>
+            </div>
+          </div>
 
-    <div className="product">
-      <div className="product-image">
-        <img src={homeHeadlightImage} alt="Head Light" 
-        width="100px"
-        height="80px" />
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeSparkImage}
+                alt="Spark Plug"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>NGK Spark Plugs</h2>
+              <p>Part Number: 54321</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="product-details">
-        <h2>Head Light</h2>
-        <p>Part Number: 67890</p>
-      </div>
-    </div>
-
-    <div className="product">
-      <div className="product-image">
-        <img src={homeSparkImage} alt="Spark Plug" 
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Spark Plug</h2>
-        <p>Part Number: 54321</p>
-      </div>
-    </div>
-  </div>
-</div>
-</>
+    </>
   );
 }
 
