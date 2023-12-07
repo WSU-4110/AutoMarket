e4b4fea313ba4d358e6f0f1e15b993baf30e2a44
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-07 16:22:40 -0500

    Update SignInPage.js

--- firebase-react-app/src/pages/SignInPage/SignInPage.js
+++ firebase-react-app/src/pages/SignInPage/SignInPage.js
@@ -5,40 +5,39 @@ import './SignInPage.css';
 import Header from "./../../Header";
 
 function App() {
-
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
   const [message, setMessage] = useState("");
 
+  // Authentication state listener
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-        setCurrentUser(user);
-    });
-
+    const unsubscribe = onAuthStateChanged(auth, user => setCurrentUser(user));
     return () => unsubscribe();
   }, []);
 
-  const login = async () => {
+  const handleLogin = async () => {
     try {
       await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
       setMessage("Successfully logged in!");
-
-      setLoginEmail("");
-      setLoginPassword("");
-      
+      resetForm();
     } catch (error) {
       setMessage(`Login Error: ${error.message}`);
     }
   };
 
-  const logout = async() => {
+  const handleLogout = async () => {
     try {
       await signOut(auth);
       setMessage("Successfully logged out!");
     } catch (error) {
       setMessage(`Logout Error: ${error.message}`);
     }
+  };
+
+  const resetForm = () => {
+    setLoginEmail("");
+    setLoginPassword("");
   };
 
   return (
@@ -50,27 +49,32 @@ function App() {
           <h2>Login</h2>
           <div className="input-container">
             <label>Email:</label>
-            <input type="email" value={loginEmail} onChange={(e) => setLoginEmail(e.target.value)} placeholder="Enter your email" />
+            <input 
+              type="email"
+              value={loginEmail}
+              onChange={e => setLoginEmail(e.target.value)}
+              placeholder="Enter your email" />
           </div>
           <div className="input-container">
             <label>Password:</label>
-            <input type="password" value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)} placeholder="Enter your password" />
+            <input 
+              type="password"
+              value={loginPassword}
+              onChange={e => setLoginPassword(e.target.value)}
+              placeholder="Enter your password" />
           </div>
-          <button className="signin-btn" onClick={login}>Login</button>
-          <br></br>
+          <button className="signin-btn" onClick={handleLogin}>Login</button>
         </div>
 
-        <div className="logout-section">
-          <h2>Logout</h2>  <br></br>
-
-          {currentUser && (
+        {currentUser && (
+          <div className="logout-section">
+            <h2>Logout</h2>
             <span className="user-info">
-              Logged in as: {currentUser?.email}
-              <br></br> <br></br>
+              Logged in as: {currentUser.email}
             </span>
-          )}
-          <button className="signin-btn" onClick={logout}>Logout</button>
-        </div>
+            <button className="signin-btn" onClick={handleLogout}>Logout</button>
+          </div>
+        )}
 
         {message && <div className="message">{message}</div>}
       </div>
