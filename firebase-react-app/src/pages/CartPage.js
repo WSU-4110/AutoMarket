02f5ca2bebcf1dac08ba6de09a6da5661db255f5
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-07 16:21:08 -0500

    Update CartPage.js

--- firebase-react-app/src/pages/CartPage.js
+++ firebase-react-app/src/pages/CartPage.js
@@ -1,68 +1,46 @@
+import React from 'react';
 import Header from "./../Header";
-import React, { useState } from 'react';
 import './CartPage.css';
-import homeBreakPadsImage from "../images/homeBreakPadsImage.jpg"
-import homeHeadlightImage from "../images/homeHeadlightImage.jpg"
-import homeSparkImage from "../images/homeSparkImage.jpg"
+import homeBreakPadsImage from "../images/homeBreakPadsImage.jpg";
+import homeHeadlightImage from "../images/homeHeadlightImage.jpg";
+import homeSparkImage from "../images/homeSparkImage.jpg";
+
 function CartPage() {
+  // Function to render a single product
+  const renderProduct = (image, title, partNumber) => (
+    <div className="product">
+      <div className="product-image">
+        <img src={image} alt={title} width="100px" height="80px" />
+      </div>
+      <div className="product-details">
+        <h2>{title}</h2>
+        <p>Part Number: {partNumber}</p>
+      </div>
+    </div>
+  );
 
   return (
     <>
-    <Header/>
-  <div className="shoppingcart-container">
-    <div className='MyCart-container'>
-     <h1 title="cart-title">MY CART</h1>
-    </div>
+      <Header/>
+      <div className="shoppingcart-container">
+        <div className='MyCart-container'>
+          <h1 title="cart-title">MY CART</h1>
+        </div>
 
-    <br></br>
+        <div className="product-container">
+          <div className='order-summary-container'>
+            <h2 title='order-title'>Order Summary</h2>
+          </div>
+          <div className='order-total-container'>
+            <h2 title='subtotal-title'>Item(s) Subtotal:</h2> 
+          </div>
 
-  <div className="product-container">
-
-    <div className='order-summary-container'>
-      <h2 title='order-title'>Order Summary</h2>
-    </div>
-    <div className='order-total-container'>
-      <h2 title='subtotal-title'>Item(s) Subtotal:</h2> 
-    </div>
-
-    <div className="product">
-      <div className="product-image">
-        <img src={homeBreakPadsImage} alt="Break Pads"
-        width="100px"
-        height="80px" />
+          {renderProduct(homeBreakPadsImage, "Break Pads", "12345")}
+          {renderProduct(homeHeadlightImage, "Head Light", "67890")}
+          {renderProduct(homeSparkImage, "Spark Plug", "54321")}
+        </div>
       </div>
-      <div className="product-details">
-        <h2>Break Pads</h2>
-        <p>Part Number: 12345</p>
-      </div>
-    </div>
-
-    <div className="product">
-      <div className="product-image">
-        <img src={homeHeadlightImage} alt="Head Light" 
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Head Light</h2>
-        <p>Part Number: 67890</p>
-      </div>
-    </div>
-
-    <div className="product">
-      <div className="product-image">
-        <img src={homeSparkImage} alt="Spark Plug" 
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Spark Plug</h2>
-        <p>Part Number: 54321</p>
-      </div>
-    </div>
-  </div>
-</div>
-</>
+    </>
   );
 }
 
