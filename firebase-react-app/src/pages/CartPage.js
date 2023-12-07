abc81c51adab79b4014073ec8be0215c66c96c05
usmansaiyed02 <saiyedusman@gmail.com>
2023-12-06 22:41:26 -0500

    Added order summary and total. Overall page fix.

--- firebase-react-app/src/pages/CartPage.js
+++ firebase-react-app/src/pages/CartPage.js
@@ -1,68 +1,110 @@
-import Header from "./../Header";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './../Header';
 import './CartPage.css';
-import homeBreakPadsImage from "../images/homeBreakPadsImage.jpg"
-import homeHeadlightImage from "../images/homeHeadlightImage.jpg"
-import homeSparkImage from "../images/homeSparkImage.jpg"
+import homeBreakPadsImage from '../images/homeBreakPadsImage.jpg';
+import homeHeadlightImage from '../images/homeHeadlightImage.jpg';
+import homeSparkImage from '../images/homeSparkImage.jpg';
+
 function CartPage() {
+  // Define the product data with prices
+  const products = [
+    { name: 'Break Pads', price: 32.49 },
+    { name: 'Morimoto XB LED Headlights', price: 89.99 },
+    { name: 'NGK Spark Plugs', price: 9.19 },
+  ];
+
+  // State to hold the subtotal
+  const [subtotal, setSubtotal] = useState(0);
+
+   // Calculate the subtotal when the component mounts or when products change
+   useEffect(() => {
+    const total = products.reduce((acc, product) => acc + product.price, 0);
+    setSubtotal(total);
+  }, [products]);
 
   return (
     <>
-    <Header/>
-  <div className="shoppingcart-container">
-    <div className='MyCart-container'>
-     <h1 title="cart-title">MY CART</h1>
-    </div>
+      <Header />
+      <div className="shoppingcart-container">
+        <div className="MyCart-container">
+        <h1 title="cart-title">MY <span>CART</span></h1>
+        </div>
 
-    <br></br>
+        <br />
 
-  <div className="product-container">
+        <div className="product-container">
+          <div className="order-summary-container">
+            <h2 title="order-title">Order Summary</h2>
+          </div>
+          <div className="order-total-container">
+            <h2 title="subtotal-title">Item(s) Subtotal:</h2>
+          </div>
 
-    <div className='order-summary-container'>
-      <h2 title='order-title'>Order Summary</h2>
-    </div>
-    <div className='order-total-container'>
-      <h2 title='subtotal-title'>Item(s) Subtotal:</h2> 
-    </div>
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeBreakPadsImage}
+                alt="Break Pads"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>Break Pads</h2>
+              <p>Part Number: 12345</p>
+            </div>
+            <div className="product-price">
+              $32.49 {/* Replace this with the actual price */}
+            </div>
+          </div>
 
-    <div className="product">
-      <div className="product-image">
-        <img src={homeBreakPadsImage} alt="Break Pads"
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Break Pads</h2>
-        <p>Part Number: 12345</p>
-      </div>
-    </div>
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeHeadlightImage}
+                alt="Head Light"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>Morimoto XB LED Headlights</h2>
+              <p>Part Number: 67890</p>
+            </div>
+            <div className="product-price">
+              $89.99 {/* Replace this with the actual price */}
+            </div>
+          </div>
 
-    <div className="product">
-      <div className="product-image">
-        <img src={homeHeadlightImage} alt="Head Light" 
-        width="100px"
-        height="80px" />
+          <div className="product">
+            <div className="product-image">
+              <img
+                src={homeSparkImage}
+                alt="Spark Plug"
+                width="100px"
+                height="80px"
+              />
+            </div>
+            <div className="product-details">
+              <h2>NGK Spark Plugs</h2>
+              <p>Part Number: 54321</p>
+            </div>
+            <div className="product-price">
+              $9.19 {/* Replace this with the actual price */}
+            </div>
+          </div>
+          <div className="subtotal-box">
+            <h2>SUBTOTAL</h2>
+            <div className="subtotal-total">${subtotal.toFixed(2)}</div>
+            <p className="small-text">Taxes and shipping calculated at checkout</p>
+          </div>
+          <div className="checkout-container">
+          <h2>Checkout</h2>
+          {/* Add any checkout-related content here */}
+        </div>
+        </div>
       </div>
-      <div className="product-details">
-        <h2>Head Light</h2>
-        <p>Part Number: 67890</p>
-      </div>
-    </div>
-
-    <div className="product">
-      <div className="product-image">
-        <img src={homeSparkImage} alt="Spark Plug" 
-        width="100px"
-        height="80px" />
-      </div>
-      <div className="product-details">
-        <h2>Spark Plug</h2>
-        <p>Part Number: 54321</p>
-      </div>
-    </div>
-  </div>
-</div>
-</>
+    </>
   );
 }
 
