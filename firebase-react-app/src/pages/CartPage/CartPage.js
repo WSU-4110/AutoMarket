5fb11e0f0a676909ec412017e976207742eef282
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-08 21:09:52 -0500

    Update CartPage.js

--- firebase-react-app/src/pages/CartPage/CartPage.js
+++ firebase-react-app/src/pages/CartPage/CartPage.js
@@ -1,55 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import Header from './../../Header';
 import './CartPage.css';
 import { useCart } from './CartContext';
 import NoPhotoAvailable from '../../images/NoPhotoAvailable.jpg';
 
-
 function CartPage() {
   const { cartItems, removeFromCart } = useCart();
-  const [subtotal, setSubtotal] = useState(0);
 
-  useEffect(() => {
-    const total = cartItems.reduce((acc, item) => {
-      const itemPrice = parseFloat(item.price) || 0; 
+  const subtotal = useMemo(() => {
+    return cartItems.reduce((acc, item) => {
+      const itemPrice = parseFloat(item.price) || 0;
       return acc + itemPrice;
     }, 0);
-    setSubtotal(total);
   }, [cartItems]);
-  
 
   return (
     <>
       <Header />
-      <div className="shoppingcart-container">
+      <main className="shoppingcart-container">
         <h1 title="cart-title">MY CART</h1>
 
-        <div className="product-container">
-          {cartItems.map((item, index) => (
-            <div key={index} className="product">
-              <div className="product-image">
-                <img
-                  src={item.imageUrl || NoPhotoAvailable}
-                  alt={item.partName}
-                  width="100px"
-                  height="80px"
-                />
+        {cartItems.length > 0 ? (
+          <div className="product-container">
+            {cartItems.map((item, index) => (
+              <article key={item.id || index} className="product">
+                <div className="product-image">
+                  <img
+                    src={item.imageUrl || NoPhotoAvailable}
+                    alt={item.partName}
+                    className="cart-product-image"
+                  />
+                </div>
+                <div className="product-details">
+                  <h2>{item.partName}</h2>
+                  <p>Price: ${item.price}</p>
+                </div>
+                <button 
+                  onClick={() => removeFromCart(item.id || index)} 
+                  className="remove-button"
+                  aria-label={`Remove ${item.partName} from cart`}>
+                    Remove
+                </button>
+              </article>
+            ))}
+            <div className="subtotal-box">
+              <h2>SUBTOTAL</h2>
+              <div className="subtotal-total">
+                ${subtotal.toFixed(2)}
               </div>
-              <div className="product-details">
-                <h2>{item.partName}</h2>
-                <p>Price: ${item.price}</p>
-              </div>
-              <button onClick={() => removeFromCart(index)} className="remove-button">Remove</button>
-            </div>
-          ))}
-          <div className="subtotal-box">
-            <h2>SUBTOTAL</h2>
-            <div className="subtotal-total">
-              ${typeof subtotal === 'number' ? subtotal.toFixed(2) : '0.00'}
             </div>
           </div>
-        </div>
-      </div>
+        ) : (
+          <p>Your cart is empty.</p>
+        )}
+      </main>
     </>
   );
 }
