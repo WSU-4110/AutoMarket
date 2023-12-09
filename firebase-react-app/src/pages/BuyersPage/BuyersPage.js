fdef0a6ef96cc30c8a55c835a69dfbc24ad9c93d
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-08 21:03:45 -0500

    Update BuyersPage.js

--- firebase-react-app/src/pages/BuyersPage/BuyersPage.js
+++ firebase-react-app/src/pages/BuyersPage/BuyersPage.js
@@ -5,58 +5,53 @@ import Header from './../../Header';
 import Footer from "./../../Footer";
 import NoPhotoAvailable from '../../images/NoPhotoAvailable.jpg';
 import { useCart } from '../CartPage/CartContext';
+import PartItem from './PartItem'; // Assuming you create a new component
 
-function MainBuyersPage() {
+// Custom hook for fetching parts
+const useFetchParts = () => {
   const [parts, setParts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const { addToCart } = useCart();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     readPartsData((fetchedParts) => {
       setParts(fetchedParts);
       setIsLoading(false);
+    }).catch(error => {
+      setError(error);
+      setIsLoading(false);
     });
   }, []);
+
+  return { parts, isLoading, error };
+};
+
+function MainBuyersPage() {
+  const { parts, isLoading, error } = useFetchParts();
+  const { addToCart } = useCart();
 
   const handleAddToCart = (part) => {
     console.log("Added to cart:", part);
     addToCart(part);
   };
-  
+
+  if (isLoading) return <p>Loading parts...</p>;
+  if (error) return <p>Error loading parts: {error.message}</p>;
 
   return (
     <div className="buyers-container">
       <Header />
       <h1>Available Parts</h1>
-      {isLoading ? (
-        <p>Loading parts...</p>
-      ) : (
-        <div className="parts-list">
-          {parts.length > 0 ? (
-            parts.map(part => (
-              <div key={part.id} className="part-item">
-                <h2>{part.partName}</h2>
-                <img 
-                  src={part.imageUrl || NoPhotoAvailable}
-                  alt={part.partName} 
-                  className="part-image"
-                />
-                <div className="part-details">
-                  <p>Category: {part.category}</p>
-                  <p>Fits: {part.fits}</p>
-                  <p>Price: ${part.price}</p>
-                  <button onClick={() => handleAddToCart(part)} className="add-to-cart-btn">
-                    Add to Cart
-                  </button>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>No parts available at the moment.</p>
-          )}
-        </div>
-      )}
-        <Footer />
+      <div className="parts-list">
+        {parts.length > 0 ? (
+          parts.map(part => (
+            <PartItem key={part.id} part={part} onAddToCart={handleAddToCart} />
+          ))
+        ) : (
+          <p>No parts available at the moment.</p>
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
