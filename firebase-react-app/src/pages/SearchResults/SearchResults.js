6bea8ae0a1527c43741bdad0857e2913a39b4235
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-08 21:16:19 -0500

    Update SearchResults.js

--- firebase-react-app/src/pages/SearchResults/SearchResults.js
+++ firebase-react-app/src/pages/SearchResults/SearchResults.js
@@ -1,15 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import { SearchContext } from './SearchContext';
 import NoPhotoAvailable from '../../images/NoPhotoAvailable.jpg';
 import Header from "./../../Header";
 
+const PartItem = ({ part, onAddToCart }) => (
+  <div key={part.id} className="part-item">
+    <h2>{part.partName}</h2>
+    <img 
+      src={part.imageUrl || NoPhotoAvailable}
+      alt={part.partName} 
+      loading="lazy"
+      className="part-image"
+    />
+    <div className="part-details">
+      <p>Category: {part.category}</p>
+      <p>Fits: {part.fits}</p>
+      <p>Price: ${part.price}</p>
+      <button onClick={() => onAddToCart(part.id)} className="add-to-cart-btn" aria-label={`Add ${part.partName} to cart`}>
+        Add to Cart
+      </button>
+    </div>
+  </div>
+);
+
 const SearchResults = () => {
   const { searchResults } = useContext(SearchContext);
 
-  const handleAddToCart = (partId) => {
-    // Logic to handle adding the part to the cart
+  const handleAddToCart = useCallback((partId) => {
     console.log("Added to cart:", partId);
-  };
+    // Additional logic to handle adding the part to the cart
+  }, []);
 
   return (
     <div className="buyers-container">
@@ -18,22 +38,7 @@ const SearchResults = () => {
       <div className="parts-list">
         {searchResults.length > 0 ? (
           searchResults.map(part => (
-            <div key={part.id} className="part-item">
-              <h2>{part.partName}</h2>
-              <img 
-                src={part.imageUrl || NoPhotoAvailable}
-                alt={part.partName} 
-                className="part-image"
-              />
-              <div className="part-details">
-                <p>Category: {part.category}</p>
-                <p>Fits: {part.fits}</p>
-                <p>Price: ${part.price}</p>
-                <button onClick={() => handleAddToCart(part.id)} className="add-to-cart-btn">
-                  Add to Cart
-                </button>
-              </div>
-            </div>
+            <PartItem key={part.id} part={part} onAddToCart={handleAddToCart} />
           ))
         ) : (
           <p>No results found.</p>
