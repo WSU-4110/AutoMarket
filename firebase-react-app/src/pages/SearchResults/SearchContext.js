5405e0b0565a267013859c903c28f695818ee20d
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-08 21:15:06 -0500

    Update SearchContext.js

--- firebase-react-app/src/pages/SearchResults/SearchContext.js
+++ firebase-react-app/src/pages/SearchResults/SearchContext.js
@@ -1,13 +1,39 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useReducer, useMemo } from 'react';
+import PropTypes from 'prop-types';
 
-export const SearchContext = createContext();
+export const SearchContext = createContext({
+  searchResults: [],
+  setSearchResults: () => {}
+});
+
+const searchReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_RESULTS':
+      return { ...state, searchResults: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const SearchProvider = ({ children }) => {
-  const [searchResults, setSearchResults] = useState([]);
+  const [state, dispatch] = useReducer(searchReducer, { searchResults: [] });
+
+  const setSearchResults = (results) => {
+    dispatch({ type: 'SET_RESULTS', payload: results });
+  };
+
+  const contextValue = useMemo(() => ({
+    searchResults: state.searchResults,
+    setSearchResults
+  }), [state.searchResults]);
 
   return (
-    <SearchContext.Provider value={{ searchResults, setSearchResults }}>
+    <SearchContext.Provider value={contextValue}>
       {children}
     </SearchContext.Provider>
   );
+};
+
+SearchProvider.propTypes = {
+  children: PropTypes.node.isRequired
 };
