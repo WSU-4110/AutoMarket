1249ec96daf2ec10837ca78a28e70f453dfa1d56
AshrafS01 <143763048+AshrafS01@users.noreply.github.com>
2023-12-08 21:06:25 -0500

    Update CartContext.js

--- firebase-react-app/src/pages/CartPage/CartContext.js
+++ firebase-react-app/src/pages/CartPage/CartContext.js
@@ -1,27 +1,45 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useReducer, useContext, useMemo } from 'react';
+import PropTypes from 'prop-types';
 
-const CartContext = createContext({
-  cartItems: [],
-  addToCart: () => {}
-});
+const CartContext = createContext();
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+      // Optionally, prevent duplicates or increase quantity here
+      return [...state, action.item];
+    case 'REMOVE':
+      return state.filter(item => item.id !== action.id);
+    case 'CLEAR':
+      return [];
+    default:
+      return state;
+  }
+};
 
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-    const [cartItems, setCartItems] = useState([]);
-  
-    const addToCart = (item) => {
-      setCartItems(prevItems => [...prevItems, item]);
-    };
-  
-    const removeFromCart = (index) => {
-      setCartItems(prevItems => prevItems.filter((_, idx) => idx !== index));
-    };
-  
+    const [cartItems, dispatch] = useReducer(cartReducer, []);
+
+    const addToCart = item => dispatch({ type: 'ADD', item });
+    const removeFromCart = id => dispatch({ type: 'REMOVE', id });
+    const clearCart = () => dispatch({ type: 'CLEAR' });
+
+    const contextValue = useMemo(() => ({
+      cartItems,
+      addToCart,
+      removeFromCart,
+      clearCart
+    }), [cartItems]);
 
     return (
-        <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+        <CartContext.Provider value={contextValue}>
           {children}
         </CartContext.Provider>
   );
+};
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired
 };
